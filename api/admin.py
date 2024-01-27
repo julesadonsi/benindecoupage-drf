21d06668c16feaeba15f7b822c4b8938c8f58f2a
JulesAD96 <adonsijules9@gmail.com>
2024-01-27 22:50:32 +0100

    Wip:Save model in admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Department, District, Neighborhoods, Town
+
+admin.site.register(Department)
+admin.site.register(Town)
+admin.site.register(District)
+admin.site.register(Neighborhoods)
--- app/urls.py
+++ app/urls.py
@@ -7,9 +7,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Bénin decoupage",
         default_version='v1',
-        description="Test description",
+        description="",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
